29206e7b8cba83b110874852580b183bc3d4ec0e
vcu-DBaptisteMitchell <78172355+vcu-DBaptisteMitchell@users.noreply.github.com>
2021-03-07 21:09:23 -0500

    Change2
    added ) to line 21
    
    changed add_list_numbers

--- fun/homework.py
+++ fun/homework.py
@@ -6,7 +6,7 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    print("The greatest number is:", max(homework.find_greatest_number))
+    print("The greatest number is:", max(find_greatest_number))
 
 
 
@@ -15,7 +15,8 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    print("The smallest number is:", (min(homework.find_least_number))
+    print("The smallest number is:", (min(find_least_number))
+    )
 
 
 def add_list_numbers(incoming_list):
@@ -23,19 +24,13 @@ def add_list_numbers(incoming_list):
     Required parameter incoming_list, should be a list.
     Add all the values together and return it.
     """
-list3= [1,2,3,4]
-    if (size ==0):
-        return 0 
-      else:
-          return homework.add_list_numbers[size - 1] + add_list_numbers(homework.add_list_numbers, size - 1)
-total = homework.add_list_numbers(add_list_numbers, len(list3))
-
-print("The sum of the list is:", total)
+    total = sum(add_list_numbers)
+    print("The sume of this list is:", total)
 
 def longest_value_key(incoming_dict):
     """
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    longest_value_key = max(homework.longest_value_key, key=len)
+    longest_value_key = max(longest_value_key, key=len)
     print(longest_value_key)
