ad21d2f9b4b747d011d82a6974998a39a68c95d2
vcu-DBaptisteMitchell <78172355+vcu-DBaptisteMitchell@users.noreply.github.com>
2021-03-08 21:24:43 -0500

    Change15

--- fun/homework.py
+++ fun/homework.py
@@ -1,37 +1,41 @@
 """Homework file for my students to have fun with some algorithms! """
 
+
 def find_greatest_number(incoming_list):
     """
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    #Return the value 
+    # Return the value
     find_greatest_number = max(incoming_list)
     return find_greatest_number
+
 
 def find_least_number(incoming_list):
     """
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    #Return the value
+    # Return the value
     find_least_number = min(incoming_list)
     return find_least_number
-    
+
+
 def add_list_numbers(incoming_list):
     """
     Required parameter incoming_list, should be a list.
     Add all the values together and return it.
     """
-    #Return the value
+    # Return the value
     add_list_numbers = sum(incoming_list)
-    return add_list_numbers  
+    return add_list_numbers
+
 
 def longest_value_key(incoming_dict):
     """
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    #Return the value
-    longest_key = max(longest_value_key, longest_value_key, key = len)
+    # Return the value
+    longest_key = max(longest_value_key, longest_value_key, key=len)
     return longest_key
